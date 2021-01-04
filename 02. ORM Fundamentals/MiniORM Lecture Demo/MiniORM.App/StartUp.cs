b69c64ed47afc77fbaf56e2dae5c70bee6532fbd
Georgi Vasilev <goshko09@gmail.com>
2021-01-04 21:50:00 +0200

    Added orm fundamentals exercise

---
+++ 02. ORM Fundamentals/MiniORM Lecture Demo/MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniORM.App
+{
+    using System.Linq;
+    using MiniORM.App.Data;
+    using MiniORM.App.Data.Entities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var connectionString = "Server=./;Database=MiniORM;Integrated Security=true";
+
+            SoftUniDbContext context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true,
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
