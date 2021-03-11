14517fe6ba9138076fbf641f3102544b2be445e1
Georgi Vasilev <goshko09@gmail.com>
2021-03-11 19:33:46 +0200

    Add exam preparation

---
+++ 13. Exam Preparation/BookShop/BookShop/Data/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Enums;
+
+    public class Book
+    {
+        public Book()
+        {
+            this.AuthorBook = new HashSet<AuthorBook>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Pages { get; set; }
+
+        public DateTime PublishedOn { get; set; }
+
+        public virtual ICollection<AuthorBook> AuthorBook { get; set; }
+    }
+}
---
+++ 13. Exam Preparation/BookShop/BookShop/DataProcessor/ExportDto/ExportBookDto.cs
@@ -0,0 +1,18 @@
+﻿namespace BookShop.DataProcessor.ExportDto
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Book")]
+    public class ExportBookDto
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Date")]
+        public string Date { get; set; }
+
+        [XmlAttribute("Pages")]
+        public int Pages { get; set; }
+
+    }
+}
